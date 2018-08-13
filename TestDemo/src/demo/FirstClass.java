ddd00c1e9954fb288574d42d2b17d91699ba69aa
litiegithub <1832568768@qq.com>
2018-08-14 01:42:51 +0800

    gg

--- TestDemo/src/demo/FirstClass.java
+++ TestDemo/src/demo/FirstClass.java
@@ -4,7 +4,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 
 		System.out.println("first....aaa");
-		System.out.println("first");
+		System.out.println("fircccst");
 
 	}
 }
